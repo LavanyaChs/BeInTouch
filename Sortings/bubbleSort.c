5b478a9dc19b67c26284f7e67568503fceb5dc1d
Sai Lavanya <sailavanya.cheemakurthy@adp.com>
2018-07-01 16:54:20 +0530

    Bubble sort with sample inputs and outputs

--- Sortings/bubbleSort.c
+++ Sortings/bubbleSort.c
@@ -22,9 +22,8 @@ int main()
                 array[j+1]=temp;
             }
         }
-        if(counter==0){
+        if(counter==0 && i==(n-1)){
             printf("\nAlready List is sorted");
-            //optimizing code comparisions
             break;
         }
     }
@@ -34,3 +33,18 @@ int main()
     }
     return 0;
 }
+/* 
+Input 1:
+14
+3 4 3 2 45 29 39 0 8 4 3 28 76 11
+Output 1:
+3 4 3 2 45 29 39 0 8 4 3 28 76 11 
+Sorted List0 2 3 3 3 4 4 8 11 28 29 39 45 76 
+Input 2:
+5
+5 7 33 56 88
+Output 2:
+5 7 33 56 88 
+Already List is sorted
+Sorted List5 7 33 56 88 
+*/
